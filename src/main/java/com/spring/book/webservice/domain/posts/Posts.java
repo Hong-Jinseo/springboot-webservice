b80cf78560e3ba6fa33b02d611fe205de12d9a43
Jinseo <2000jinseo@gmail.com>
2023-08-06 20:48:24 +0900

    [feat] 생성시간/수정시간 자동화 - 스프링 부트와 AWS로 혼자 구현하는 웹 서비스 2장 (79-118p) - JPA Auditing - BaseTimeEntity class

--- src/main/java/com/spring/book/webservice/domain/posts/Posts.java
+++ src/main/java/com/spring/book/webservice/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.spring.book.webservice.domain.posts;
 
+import com.spring.book.webservice.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,7 +20,7 @@ import javax.persistence.Id;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
