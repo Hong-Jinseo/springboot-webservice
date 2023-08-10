d100547c5b3b96fff04555614d696ac3eb5e8740
Jinseo <2000jinseo@gmail.com>
2023-08-10 22:47:02 +0900

    [feat] 게시글 조회/수정/삭제 화면 생성 - 스프링 부트와 AWS로 혼자 구현하는 웹 서비스 4장 (145-162p) - mustache

--- src/main/java/com/spring/book/webservice/service/PostsService.java
+++ src/main/java/com/spring/book/webservice/service/PostsService.java
@@ -2,6 +2,7 @@ package com.spring.book.webservice.service;
 
 import com.spring.book.webservice.domain.posts.Posts;
 import com.spring.book.webservice.domain.posts.PostsRepository;
+import com.spring.book.webservice.web.dto.PostsListResponseDto;
 import com.spring.book.webservice.web.dto.PostsResponseDto;
 import com.spring.book.webservice.web.dto.PostsSaveRequestDto;
 import com.spring.book.webservice.web.dto.PostsUpdateRequestDto;
@@ -10,6 +11,9 @@ import org.springframework.stereotype.Service;
 
 import org.springframework.transaction.annotation.Transactional;
 //import javax.transaction.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -31,5 +35,20 @@ public class PostsService {
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id="+id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                // .map(posts -> new PostsListResponseDto(posts))와 동일
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        postsRepository.delete(posts);
     }
 }
