1d4f34c0e0a80811f5470eeaaf24721a27e49600
Jinseo <2000jinseo@gmail.com>
2023-08-14 23:00:49 +0900

    [feat] 구글 연동 로그인 - 스프링 부트와 AWS로 혼자 구현하는 웹 서비스 5장 (163-194p) - OAuth 2.0 - Spring Security

--- src/main/java/com/spring/book/webservice/web/IndexController.java
+++ src/main/java/com/spring/book/webservice/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.spring.book.webservice.web;
 
+import com.spring.book.webservice.config.auth.dto.SessionUser;
 import com.spring.book.webservice.service.PostsService;
 import com.spring.book.webservice.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +9,25 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        // 로그인
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
